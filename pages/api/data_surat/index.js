899cbb4c44fac57331570f89ac798a63ae3429b9
thisisnadya <nadyamumtazah33@gmail.com>
2023-05-06 11:25:58 +0700

    comment data_surat/index

--- pages/api/data_surat/index.js
+++ pages/api/data_surat/index.js
@@ -1,34 +1,34 @@
-import connectMongo from "@/database/conn";
-import {
-  getAllSuratMasuk,
-  getDetailSurat,
-  uploadSurat,
-} from "@/database/controller";
+// import connectMongo from "@/database/conn";
+// import {
+//   getAllSuratMasuk,
+//   getDetailSurat,
+//   uploadSurat,
+// } from "@/database/controller";
 
-export default function handler(req, res) {
-  connectMongo().catch(() => {
-    res.status(405).json({ error: "Mongo connection error" });
-  });
+// export default function handler(req, res) {
+//   connectMongo().catch(() => {
+//     res.status(405).json({ error: "Mongo connection error" });
+//   });
 
-  const { method, query } = req;
-  console.log(req.query);
+//   const { method, query } = req;
+//   console.log(req.query);
 
-  switch (method) {
-    case "GET":
-      getAllSuratMasuk(req, res);
-      break;
-    case "POST":
-      uploadSurat(req, res);
-      break;
-    case "PUT":
-      res.send("PUT request");
-      break;
-    case "DELETE":
-      res.send("DELETE request");
-      break;
+//   switch (method) {
+//     case "GET":
+//       getAllSuratMasuk(req, res);
+//       break;
+//     case "POST":
+//       uploadSurat(req, res);
+//       break;
+//     case "PUT":
+//       res.send("PUT request");
+//       break;
+//     case "DELETE":
+//       res.send("DELETE request");
+//       break;
 
-    default:
-      res.send("Method not allowed");
-      break;
-  }
-}
+//     default:
+//       res.send("Method not allowed");
+//       break;
+//   }
+// }
