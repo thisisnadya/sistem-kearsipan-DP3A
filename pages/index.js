8bb5cc800f8bd0ea8c7a53df847195d44dee5913
thisisnadya <nadyamumtazah33@gmail.com>
2023-05-06 19:29:49 +0700

    comment session

--- pages/index.js
+++ pages/index.js
@@ -36,9 +36,9 @@ export default function Home() {
   const router = useRouter();
   // console.log(session);
 
-  useEffect(() => {
-    if (session.status == "unauthenticated") router.replace("/auth/login");
-  }, [session.status]);
+  // useEffect(() => {
+  //   if (session.status == "unauthenticated") router.replace("/auth/login");
+  // }, [session.status]);
 
   useEffect(async () => {
     setChartData(await getChartData());
